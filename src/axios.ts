66a5756bd17fceb4bf26b830b7c22334ef7b99da
Jinlong Peng <jinlong.peng@merico.dev>
2022-08-06 23:26:05 +0800

    use REACT_APP_API_URL

--- src/axios.ts
+++ src/axios.ts
@@ -1,5 +1,5 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: process.env.REACT_APP_LONGURL_SERVER || "http://localhost:3000",
+  baseURL: process.env.REACT_APP_API_URL || "http://localhost:3000",
 });
